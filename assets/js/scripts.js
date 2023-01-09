f8bd315d7f56faa9a571a7731757cdd5e9afe93f
Stanislav Kushnir <must3r@gmail.com>
2023-01-09 13:14:10 +0200

    prodct card for desktop in process

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,3 +4,22 @@ $(document).ready(function(){
     dots: true
   });
 });
+
+// Carousel
+
+// Carousel end
+
+let size = $('.product-size .button.active').val()
+let color = $('.product-color .button.active').val()
+$('.product-size .button').click(function() {
+  $(this).addClass('active')
+  $(this).siblings().removeClass('active')
+  size = $(this).val()
+  console.log(size)
+})
+$('.product-color .button').click(function() {
+  $(this).addClass('active')
+  $(this).siblings().removeClass('active')
+  color = $(this).val()
+  console.log(color)
+})
--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,22 @@ $(document).ready(function () {
     dots: true
   });
 });
+
+// Carousel
+
+// Carousel end
+
+var size = $('.product-size .button.active').val();
+var color = $('.product-color .button.active').val();
+$('.product-size .button').click(function () {
+  $(this).addClass('active');
+  $(this).siblings().removeClass('active');
+  size = $(this).val();
+  console.log(size);
+});
+$('.product-color .button').click(function () {
+  $(this).addClass('active');
+  $(this).siblings().removeClass('active');
+  color = $(this).val();
+  console.log(color);
+});
